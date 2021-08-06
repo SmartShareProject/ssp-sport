670f377ddf7ea7fc877b7d1e0cf93f5baf277ba8
ssp-dev <github@smartshare.vip>
2021-08-06 20:10:19 +0800

    update Hessian Utils

--- src/main/java/com/ssp/api/hessian/HessianHeaderContext.java
+++ src/main/java/com/ssp/api/hessian/HessianHeaderContext.java
@@ -5,39 +5,39 @@ import java.util.Map;
 
 public class HessianHeaderContext {
 
-    private static final ThreadLocal<HessianHeaderContext> THREAD_LOCAL = new ThreadLocal<>();
+  private static final ThreadLocal<HessianHeaderContext> THREAD_LOCAL = new ThreadLocal<>();
 
-    private Map<String, String>                            headers      = new HashMap<>();
+  private final Map<String, String> headers = new HashMap<>();
 
-    private HessianHeaderContext() {
+  private HessianHeaderContext() {
+  }
+
+  public static HessianHeaderContext getContext() {
+    HessianHeaderContext context = THREAD_LOCAL.get();
+    if (context == null) {
+      context = new HessianHeaderContext();
+      THREAD_LOCAL.set(context);
     }
+    return context;
+  }
 
-    public static HessianHeaderContext getContext() {
-        HessianHeaderContext context = THREAD_LOCAL.get();
-        if (context == null) {
-            context = new HessianHeaderContext();
-            THREAD_LOCAL.set(context);
-        }
-        return context;
-    }
+  public void addHeader(String name, String value) {
+    headers.put(name, value);
+  }
 
-    public void addHeader(String name, String value) {
-        headers.put(name, value);
-    }
+  public String getHeader(String name) {
+    return headers.get(name);
+  }
 
-    public String getHeader(String name) {
-        return headers.get(name);
-    }
+  public Map<String, String> getHeaders() {
+    return headers;
+  }
 
-    public Map<String, String> getHeaders() {
-        return headers;
+  public static void close() {
+    HessianHeaderContext context = THREAD_LOCAL.get();
+    if (context != null) {
+      context.headers.clear();
+      THREAD_LOCAL.set(null);
     }
-
-    public static void close() {
-        HessianHeaderContext context = THREAD_LOCAL.get();
-        if (context != null) {
-            context.headers.clear();
-            THREAD_LOCAL.set(null);
-        }
-    }
+  }
 }
--- src/main/java/com/ssp/api/hessian/HessianProxy.java
+++ src/main/java/com/ssp/api/hessian/HessianProxy.java
@@ -6,25 +6,27 @@ import java.net.URL;
 import java.util.Map;
 
 
-public class HessianProxy extends com.caucho.hessian.client.HessianProxy  {
+public class HessianProxy extends com.caucho.hessian.client.HessianProxy {
 
-   /**  */
-   private static final long serialVersionUID = 1L;
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
 
-    protected HessianProxy(URL url, HessianProxyFactory factory) {
-       super(url, factory);
+  protected HessianProxy(URL url, HessianProxyFactory factory) {
+    super(url, factory);
+  }
+
+  protected HessianProxy(URL url, HessianProxyFactory factory, Class<?> type) {
+    super(url, factory, type);
+  }
+
+  @Override
+  protected void addRequestHeaders(HessianConnection conn) {
+    super.addRequestHeaders(conn);
+    Map<String, String> headerMap = HessianHeaderContext.getContext().getHeaders();
+    for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+      conn.addHeader(entry.getKey(), entry.getValue());
     }
-
-    protected HessianProxy(URL url, HessianProxyFactory factory, Class<?> type) {
-        super(url, factory, type);
-   }
-
-    @Override
-    protected void addRequestHeaders(HessianConnection conn) {
-        super.addRequestHeaders(conn);
-       Map<String, String> headerMap = HessianHeaderContext.getContext().getHeaders();
-        for (Map.Entry<String, String> entry : headerMap.entrySet()) {
-            conn.addHeader(entry.getKey(), entry.getValue());
-        }
-    }
+  }
 }
